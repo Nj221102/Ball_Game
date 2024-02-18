7c81256a0db95dd91f6002bd9a4f2cc483b6de00
Nj221102 <nitishj221102@gmail.com>
2024-02-18 22:53:55 +0530

    Fun but difficult game

--- src/App.js
+++ src/App.js
@@ -3,8 +3,14 @@ import "./App.css";
 
 const BallButton = () => {
   const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [win_count, setWin_count] =  useState(0)
+    
   function handleClick() {
     window.alert("you win!");
+  }
+  const onWin = () => {
+    setWin_count(win_count+1);
+    moveBall();
   }
 
   const moveBall = () => {
@@ -14,14 +20,20 @@ const BallButton = () => {
   };
 
   return (
-    <div className="ball-container">
+    <div className="body">
+      <h1>Click ball to win!</h1>
+        <div className="ball-container">
       <button
         className="ball"
         style={{ left: `${position.x}px`, top: `${position.y}px` }}
         onMouseEnter={moveBall}
-        onClick={handleClick}
+        onClick={handleClick && onWin}
+        
       ></button>
     </div>
+    <h2>Win counter : {win_count}</h2>
+    </div>
+    
   );
 };
 
